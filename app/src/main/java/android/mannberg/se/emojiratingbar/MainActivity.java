59ea8f558101597571a2c98fa66e649900f7176b
Göran Mannberg <mannberg@gmail.com>
2017-07-25 19:20:41 +0200

    Update MainActivity.java
    Spellling

--- app/src/main/java/android/mannberg/se/emojiratingbar/MainActivity.java
+++ app/src/main/java/android/mannberg/se/emojiratingbar/MainActivity.java
@@ -37,9 +37,9 @@ public class MainActivity extends AppCompatActivity {
                 switch ((int)rating) {
                     case 1: msgview.setText(R.string.msg000); msg = "Nice "+getEmojiByUnicode(0x1F4AF)+getEmojiByUnicode(0x1F44A)+getEmojiByUnicode(0x270C); break;
                     case 2: msgview.setText(R.string.msg001); msg = "Hang in there "+getEmojiByUnicode(0x1F60D)+"!! "+getEmojiByUnicode(0x1F48B); break;
-                    case 3: msgview.setText(R.string.msg002); msg = "Everything will be alright "+getEmojiByUnicode(0x1F917); break;
-                    case 4: msgview.setText(R.string.msg003); msg = "Double hugg attack!! "+getEmojiByUnicode(0x1F917)+getEmojiByUnicode(0x1F917)+getEmojiByUnicode(0x1F60A); break;
-                    case 5: msgview.setText(R.string.msg004); msg = "You are the best person I know, everything will be better and I can help you with whatever "+getEmojiByUnicode(0x1F47B); break;
+                    case 3: msgview.setText(R.string.msg002); msg = "Everything will be all right "+getEmojiByUnicode(0x1F917); break;
+                    case 4: msgview.setText(R.string.msg003); msg = "Double hug attack!! "+getEmojiByUnicode(0x1F917)+getEmojiByUnicode(0x1F917)+getEmojiByUnicode(0x1F60A); break;
+                    case 5: msgview.setText(R.string.msg004); msg = "You are the best person I know, everything will be better and I can help you with whatever "+getEmojiByUnicode(0x1F607); break;
                     case 6: msgview.setText(R.string.msg005); msg = "You little peice of "+getEmojiByUnicode(0x1F4A9)+", grow up "+getEmojiByUnicode(0x1F61C); break;
                     default: msgview.setText(R.string.msg_err); break;
                 }
